235ad26a7f3c869d37c9d38626fe6e1ba0c976a8
Юрьев Николай <n.yuriev@growfood.pro>
2026-06-28 12:25:33 +0300

    fix i18n json import for Vite (default import so lang.js gets non-identifier keys)

--- resources/js/i18n/index.js
+++ resources/js/i18n/index.js
@@ -1,6 +1,8 @@
 
 import Lang from 'lang.js';
-import * as messages from './messages.json';
+// default-импорт: Vite отдаёт весь JSON одним объектом, а `import * as` не даёт доступ
+// к ключам-неидентификаторам ("ru.common" и т.п.), из-за чего lang.js не находил переводы.
+import messages from './messages.json';
 
 const lang = new Lang({
     messages: messages,
